5dbc4e145b4c5e75517965d9efa70af3e783c635
MatteoSpoladore <matteo.spoladore99@gmail.com>
2025-06-10 20:54:43 +0200

    costrutturoe per label,entry e bottoni

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
-import sqlite3
-from components.clienti import Cliente
+
+# import sqlite3
+# from components.clienti import Cliente
 from components.mostra_clienti import ClientiView
 from components.aggiungi_cliente import aggiungi_cliente as aggiungi_cliente_fin
 import tkinter.messagebox as msg
@@ -12,65 +13,32 @@ class App(ctk.CTk):
         self.title("Registra fatture")
         self.geometry("900x500")
 
-        span_x = 50
-        label_width = 300
-        entry_width = 300
-
-        # Colonna 0
-        self.nome_label = ctk.CTkLabel(
-            master=self, text="Nome", width=label_width, justify="center"
-        )
-        self.nome_label.grid(row=0, column=0, padx=span_x, pady=(20, 0))
-
-        self.nome_entry = ctk.CTkEntry(
-            master=self, width=entry_width, placeholder_text="nome..."
-        )
-        self.nome_entry.grid(row=1, column=0, padx=span_x, pady=(0, 20))
-
-        self.cognome_label = ctk.CTkLabel(
-            master=self, text="Cognome", width=label_width, justify="center"
-        )
-        self.cognome_label.grid(row=2, column=0, padx=span_x, pady=(20, 0))
-
-        self.cognome_entry = ctk.CTkEntry(
-            master=self, width=entry_width, placeholder_text="cognome..."
-        )
-        self.cognome_entry.grid(row=3, column=0, padx=span_x, pady=(0, 20))
-
-        # Colonna 1
-        self.indirizzo_label = ctk.CTkLabel(
-            master=self, text="Indirizzo", width=label_width, justify="center"
-        )
-        self.indirizzo_label.grid(row=0, column=1, padx=span_x, pady=(20, 0))
-
-        self.indirizzo_entry = ctk.CTkEntry(
-            master=self, width=entry_width, placeholder_text="indirizzo..."
-        )
-        self.indirizzo_entry.grid(row=1, column=1, padx=span_x, pady=(0, 20))
-
-        self.azienda_label = ctk.CTkLabel(
-            master=self, text="Nome azienda", width=label_width, justify="center"
-        )
-        self.azienda_label.grid(row=2, column=1, padx=span_x, pady=(20, 0))
-
-        self.azienda_entry = ctk.CTkEntry(
-            master=self, width=entry_width, placeholder_text="nome azienda..."
-        )
-        self.azienda_entry.grid(row=3, column=1, padx=span_x, pady=(0, 20))
-
-        # Configura le colonne per ridimensionamento
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-        self.add_btn = ctk.CTkButton(
-            master=self, text="Aggiungi cliente", command=self.aggiungi_cliente
+        self.nome_entry = self.crea_label_entry("Nome", "nome...", 0, 0)
+        self.cognome_entry = self.crea_label_entry("Cognome", "cognome...", 2, 0)
+        self.indirizzo_entry = self.crea_label_entry("Indirizzo", "indirizzo...", 0, 1)
+        self.azienda_entry = self.crea_label_entry(
+            "Nome azienda", "nome azienda...", 2, 1
         )
-        self.add_btn.grid(row=4, column=0, pady=10)
 
-        self.show_btn = ctk.CTkButton(
-            master=self, text="Mostra clienti", command=self.mostra_clienti
+        self.crea_bottoni()
+
+    def crea_label_entry(self, testo, placeholder, row, col):
+        label = ctk.CTkLabel(self, text=testo, justify="center", width=300)
+        label.grid(row=row, column=col, padx=50, pady=(20, 0))
+        entry = ctk.CTkEntry(self, width=300, placeholder_text=placeholder)
+        entry.grid(row=row + 1, column=col, padx=50, pady=(0, 20))
+        return entry
+
+    def crea_bottoni(self):
+        ctk.CTkButton(
+            self, text="Aggiungi cliente", command=self.aggiungi_cliente
+        ).grid(row=4, column=0, pady=10)
+        ctk.CTkButton(self, text="Mostra clienti", command=self.mostra_clienti).grid(
+            row=4, column=1, pady=10
         )
-        self.show_btn.grid(row=4, column=1, pady=10)
 
     def aggiungi_cliente(self):
         aggiungi_cliente_fin(
